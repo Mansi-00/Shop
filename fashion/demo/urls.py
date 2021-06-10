db47083414d9e51410c979d340829df187416992
Mansi <mfefar0794@gmail.com>
2021-06-11 00:49:46 +0530

    allauth done

--- fashion/demo/urls.py
+++ fashion/demo/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from .views import Demo, Demo2
 
+app_name = 'Demo'
 
 urlpatterns = [
-    path('', Demo),
+    path('', Demo, name='home'),
     path('common', Demo2),
     
 ]
--- fashion/fashion/settings.py
+++ fashion/fashion/settings.py
@@ -187,5 +187,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = '**** your Email ****'
-EMAIL_HOST_PASSWORD = '**** Your Password ****'
+EMAIL_HOST_USER = 'Enter Your Email'
+EMAIL_HOST_PASSWORD = 'Enter Your Email Password'
